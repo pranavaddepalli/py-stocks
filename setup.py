998dc9952f7f84f1b047bd1ac2af5a3eb3c41f50
Pranav Addepalli <pranavaddepalli@gmail.com>
2020-07-01 14:13:13 -0400

    .

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from distutils.core import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='py-stocks',
     packages=['py-stocks'],
     version='0.3',
     license='MIT',
     description="py-stocks is a package for retrieving real-time stock data, historical stock data, and stock technical indicators.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Pranav Addepalli', 
     author_email='pranav.addepalli@gmail.com',
-    url='https://github.com/pranavaddepalli/PyStocks',
-    download_url='https://github.com/pranavaddepalli/PyStocks/archive/0.2.tar.gz',
+    url='https://github.com/pranavaddepalli/py-stocks',
+    download_url='https://github.com/pranavaddepalli/py-stocks/archive/v1.0.tar.gz',
     keywords=['Finance', 'Stocks', 'Python', 'Scraper'],
     install_requires=[
         'stockquotes',
